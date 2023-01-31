d8d15222c3c76f5ab16fa72c197b1ea717218177
BLUGTHEK <blugthek@gmail.com>
2023-01-31 15:30:45 +0700

    commit : Change from standalone script to collection of script that does the same thing

---
+++ BX_GorvVault/config.lua
@@ -0,0 +1,27 @@
+tempDataStorage = {}
+Config = {} or Config
+
+Config.Vault = {
+    Vault = {coords = vector3(-2074.9, -1017.54, 11.91), 
+             dist = 1,
+             job = 'gouvernment',
+             sqlQuery = 'society_gorv'
+             
+    },
+    Vault2 = {coords = vector3(-469.17, 1129.27, 325.86),
+              dist = 1,
+              job = 'gouvernment',
+              sqlQuery = 'society_gorv'
+              
+    },
+    vPolice1 = {coords = vector3(450.66, -989.67, 30.69),
+                dist = 1,
+                job = 'police',
+                sqlQuery = 'society_police'
+
+    },
+}
+
+
+
+
---
+++ BX_GorvVault/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'BLUGTHEK Simple Vault'
+version '1.0b'
+author 'BLUGTHEK'
+--[[
+Change log
+1.0a
+Simple Government Vault
+
+1.0b
+Make change to scripts to make it more Flexible and more Configurable
+Change name to 'Simple Vault'
+]]
+
+client_scripts {
+	'config.lua',
+	'client.lua',
+}
